1aa273c7b1b7cea58a2351670310407d378e2e5d
Marco Muser <marco.muser@posteo.de>
2021-01-27 17:43:01 +0100

    fetch images from strapi backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,39 +1,33 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { getSliderImages } from "./services/slider.service";
+import Slider from "./Slider";
 
 interface AppProps {}
 
 function App({}: AppProps) {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
+  const [images, setImages] = useState<object[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
   useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
+    const fetchImages = async () => {
+      try {
+        setIsLoading(true);
+        const json = await getSliderImages();
+        setImages([...json]);
+        setIsLoading(false);
+      } catch (err) {
+        setIsError(true);
+      }
+    };
+    fetchImages();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      {isError && <p>Something went wrong. Try to reload the page!</p>}
+      {isLoading ? <p>Loading...</p> : <Slider images={images} />}
     </div>
   );
 }
