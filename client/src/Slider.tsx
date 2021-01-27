bea0aa9bb3cce533e5770f2eeb9b2f622fa564d3
Marco Muser <marco.muser@posteo.de>
2021-01-27 17:43:14 +0100

    add Slider component

---
+++ client/src/Slider.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface SliderProps {
+    images: object[];
+}
+
+function Slider(props: SliderProps) {
+    return (
+        <p>Hello World</p>
+    )
+}
+
+export default Slider;
