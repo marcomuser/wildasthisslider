55087e79810731ea9ba9c39e0074030130b1c209
Marco Muser <marco.muser@posteo.de>
2021-01-30 13:56:02 +0100

    add types for image json from Strapi REST API

--- client/src/services/slider.service.tsx
+++ client/src/services/slider.service.tsx
@@ -1,4 +1,4 @@
-export async function getSliderImages() {
+export async function getSliderImages(): Promise<Image[]> {
   try {
     const response = await fetch("http://localhost:1337/slider");
     const json = await response.json();
@@ -6,4 +6,51 @@ export async function getSliderImages() {
   } catch (err) {
     throw err;
   }
+}
+
+export interface Image {
+  id: number;
+  name: string;
+  alternativeText: string;
+  caption: string;
+  width: number;
+  height: number;
+  formats: Formats;
+  hash: string;
+  ext: EXT;
+  mime: MIME;
+  size: number;
+  url: string;
+  previewUrl: null;
+  provider: string;
+  provider_metadata: null;
+  created_at: Date;
+  updated_at: Date;
+}
+
+enum EXT {
+  PNG = ".png",
+}
+
+interface Formats {
+  thumbnail: Large;
+  large: Large;
+  medium: Large;
+  small: Large;
+}
+
+interface Large {
+  name: string;
+  hash: string;
+  ext: EXT;
+  mime: MIME;
+  width: number;
+  height: number;
+  size: number;
+  path: null;
+  url: string;
+}
+
+enum MIME {
+  ImagePNG = "image/png",
 }
